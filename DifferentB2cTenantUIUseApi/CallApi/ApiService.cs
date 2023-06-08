8d975e67b1843c51c67030703b00137d16d2efc1
damienbod <damien_bod@hotmail.com>
2023-06-08 18:59:49 +0200

    Update test

---
+++ DifferentB2cTenantUIUseApi/CallApi/ApiService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Identity.Web;
+using System.Net.Http.Headers;
+
+namespace DifferentTenantUIUseApi;
+
+public class ApiService
+{
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly IConfiguration _configuration;
+
+    public ApiService(IHttpClientFactory clientFactory,
+        ITokenAcquisition tokenAcquisition,
+        IConfiguration configuration)
+    {
+        _clientFactory = clientFactory;
+        _tokenAcquisition = tokenAcquisition;
+        _configuration = configuration;
+    }
+
+    public async Task<string> GetApiDataAsync()
+    {
+        var client = _clientFactory.CreateClient();
+
+        var scope = _configuration["CallApi:ScopeForAccessToken"];
+
+        if (scope == null)
+            throw new ArgumentNullException("scope configuration not set ");
+
+        var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { scope });
+
+        string? url = _configuration["CallApi:ApiBaseAddress"];
+        if (url != null)
+            client.BaseAddress = new Uri(url);
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await client.GetAsync("Business");
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return responseContent;
+        }
+
+        throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
+    }
+}
