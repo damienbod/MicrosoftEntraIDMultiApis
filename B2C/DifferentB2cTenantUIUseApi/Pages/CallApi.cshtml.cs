784e656c1fa415bb50aab06a0ab08281bd205c2c
damienbod <damien_bod@hotmail.com>
2023-06-08 22:14:21 +0200

    Update project

--- B2C/DifferentB2cTenantUIUseApi/Pages/CallApi.cshtml.cs
+++ B2C/DifferentB2cTenantUIUseApi/Pages/CallApi.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.Identity.Web;
 
 namespace DifferentTenantUIUseApi.Pages;
 
-[AuthorizeForScopes(Scopes = new string[] { "api://98328d53-55ec-4f14-8407-0ca5ff2f2d20/access_as_user" })]
+[AuthorizeForScopes(Scopes = new string[] { "https://companytwob2c.onmicrosoft.com/ca8dc6a9-c0de-4dfb-8e42-758ef311d8ab/access_as_user" })]
 public class CallApiModel : PageModel
 {
     private readonly ApiService _apiService;
