3e6b462f1c8a65888ff3d83b86da4dbf8302fcbc
damienbod <damien_bod@hotmail.com>
2024-01-14 21:01:25 +0100

    Update packages

--- B2C/AadB2cMultiTenantApi/Controllers/BusinessController.cs
+++ B2C/AadB2cMultiTenantApi/Controllers/BusinessController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyApi.Controllers;
+namespace MeIDB2cMultiTenantApi.Controllers;
 
 [Authorize]
 [ApiController]
